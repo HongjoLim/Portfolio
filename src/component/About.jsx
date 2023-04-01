b42fb5247cfdefc607207f4373c05c9c363f6ce7
hongjolim <hongjolim@geotab.com>
2023-04-01 14:36:14 -0700

    Added about me section

---
+++ src/component/About.jsx
@@ -0,0 +1,28 @@
+import './About.css';
+import me from '../img/me.png';
+
+const About = () => {
+    return (
+    <div className="a">
+        <div className="a-left"> 
+            <div className='a-card bg'></div>
+            <div className='a-card'>
+                <img className='a-img' src={me} alt=''/>
+            </div>
+        </div>
+        <div className="a-right">
+            <div className='a-desc'>
+                <h1>About me</h1>
+                <hr/>
+                <p>During 5 years of writing code, I believe everything is possible in programming.</p>
+                <p>I love...</p>
+                <p>making my customers happy with my code</p>
+                <p>helping my colleagues solve their problems.</p>
+            </div>
+        </div>
+
+    </div>
+    );
+}
+
+export default About;
